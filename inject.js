6d03cb3dc1698d888ac25e9747165020dfc00d1f
Pavel Kurnosov <pashky@gmail.com>
2015-06-08 20:26:13 +0100

    Handle infinite loading

--- inject.js
+++ inject.js
@@ -23,42 +23,55 @@ chrome.runtime.sendMessage({init:true}, function(response) {
         return '';
       };
 
-      $('.post,#fw_post').forEach(function (post) {
+      function insertLinks() {
+        $('.post,#fw_post').forEach(function (post) {
+          if ($('.vkdlzip', post).length) return;
 
-        var audios = [];
-        var n = 1;
-        $('.audio', post).forEach(function (audio) {
-          var url = $('input[id^="audio_info"]', audio).val().split(',')[0];
-          if (url) {
-            var artist = $('b a', audio).text();
-            var title = $('span.title', audio).text();
-            audios.push({n: n, a: artist, t: title, u: url});
-            ++n;
-          }
-        });
-
-        var author = $('.wall_text .author,.fw_post_author', post).text();
-        var title = $('.wall_post_text', post).text();
-        var time = $('.rel_date', post).attr('time');
-
-        if (audios.length > 0) {
-          var linkid = 'vkdlzip' + post.id;
-          var link = $('&nbsp;-&nbsp;<a href="#" id="' + linkid + '">Download</a>');
-          $('.wall_text .author,.fw_post_author', post).parent().append(link);
-          link.on('click', function (e) {
-            chrome.runtime.sendMessage({
-              audios: audios,
-              linkid: linkid,
-              tabid: response.tabid,
-              author: author,
-              title: title,
-              time: time
-            });
-            e.preventDefault();
+          var audios = [];
+          var n = 1;
+          $('.audio', post).forEach(function (audio) {
+            var url = $('input[id^="audio_info"]', audio).val().split(',')[0];
+            if (url) {
+              var artist = $('b a', audio).text();
+              var title = $('span.title', audio).text();
+              audios.push({n: n, a: artist, t: title, u: url});
+              ++n;
+            }
           });
-        }        
-      });
+
+          var author = $('.wall_text .author,.fw_post_author', post).text();
+          var title = $('.wall_post_text', post).text();
+          var time = $('.rel_date', post).attr('time');
+
+          if (audios.length > 0) {
+            var linkid = 'vkdlzip' + post.id;
+            var link = $('&nbsp;-&nbsp;<a class="vkdlzip" href="#" id="' + linkid + '">Download</a>');
+            $('.wall_text .author,.fw_post_author', post).parent().append(link);
+            link.on('click', function (e) {
+              chrome.runtime.sendMessage({
+                audios: audios,
+                linkid: linkid,
+                tabid: response.tabid,
+                author: author,
+                title: title,
+                time: time
+              });
+              e.preventDefault();
+            });
+          }        
+        });
+      }
       
-	}
-  }, 10);
+      insertLinks();
+
+      var rows = $('.feed_row').length;
+      setInterval(function() {
+        var newrows = $('.feed_row').length;
+        if (newrows != rows) {
+          insertLinks();
+        }
+        rows = newrows;
+      }, 1000);
+    }
+  });
 });
