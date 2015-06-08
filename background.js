f49f6015b94ae5d7f722af5911700c59b2c45b55
Pavel Kurnosov <pashky@gmail.com>
2015-06-08 19:53:16 +0100

    Initial version

---
+++ background.js
@@ -0,0 +1,70 @@
+function extend(defaults, options) {
+    var extended = {};
+    var prop;
+    for (prop in defaults) {
+        if (Object.prototype.hasOwnProperty.call(defaults, prop)) {
+            extended[prop] = defaults[prop];
+        }
+    }
+    for (prop in options) {
+        if (Object.prototype.hasOwnProperty.call(options, prop)) {
+            extended[prop] = options[prop];
+        }
+    }
+    return extended;
+};
+
+function pad(num, size) {
+    var s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
+}
+
+function getBinary(file, then) {
+  var xhr = new XMLHttpRequest();  
+  xhr.open("GET", file, true);
+  xhr.responseType = "arraybuffer";  
+  xhr.send();
+  xhr.onload = function(event) {
+    then(xhr.response);
+  };
+}
+
+function loadNext(audios, r) {
+  if (audios.length == 0) {
+    var blob = r.zip.generate({type:"blob"});
+    chrome.tabs.sendMessage(r.tabid, {done:true, linkid: r.linkid});
+    
+    var time = time != '' ? new Date(parseInt(r.time) * 1000) : new Date();
+    var artists = Object.keys(r.artists).slice(0, 10).join(',');
+    var zipname = pad(time.getFullYear() % 100, 2) + '' +
+          pad(time.getMonth() + 1, 2) + '' +
+          pad(time.getDate(), 2) + 
+          ' ' + r.author + ' ' + artists + '.zip';
+
+    saveAs(blob, zipname);
+  } else {
+    var a = audios[0];
+    var fname = pad(a.n, 2) + " " + a.a + " - " + a.t + ".mp3";
+    console.log('Loading ' + fname + ' from ' + a.u);
+    chrome.tabs.sendMessage(r.tabid, {current: r.audios.length - audios.length + 1, of: r.audios.length, linkid: r.linkid});
+    if (!r.artists) {
+      r.artists = {};
+    }
+    r.artists[a.a] = 1;
+    getBinary(a.u, function (data) {
+      r.zip.file(fname, data, {binary:true});
+      loadNext(audios.slice(1), r);
+    });
+  }
+}
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.audios) {
+      loadNext(request.audios, extend(request, {zip: new JSZip()}));
+    } else if(request.init) {
+      sendResponse({tabid:sender.tab.id});
+    }
+  }
+);
---
+++ inject.js
@@ -0,0 +1,64 @@
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    var link = $('#' + request.linkid);
+    if (request.current) {
+      link.html('Downloading ' + request.current + '/' + request.of);
+    } else if (request.done) {
+      link.html('Downloaded');
+    }
+  }
+);
+
+chrome.runtime.sendMessage({init:true}, function(response) {
+  var readyStateCheckInterval = setInterval(function() {
+	if (document.readyState === "complete") {
+	  clearInterval(readyStateCheckInterval);
+
+      console.log('vkdlzip loaded');
+
+      $.fn.text = function() {
+        if (this[0]) {
+          return ('' + this[0].textContent).trim();
+        }
+        return '';
+      };
+
+      $('.post,#fw_post').forEach(function (post) {
+
+        var audios = [];
+        var n = 1;
+        $('.audio', post).forEach(function (audio) {
+          var url = $('input[id^="audio_info"]', audio).val().split(',')[0];
+          if (url) {
+            var artist = $('b a', audio).text();
+            var title = $('span.title', audio).text();
+            audios.push({n: n, a: artist, t: title, u: url});
+            ++n;
+          }
+        });
+
+        var author = $('.wall_text .author,.fw_post_author', post).text();
+        var title = $('.wall_post_text', post).text();
+        var time = $('.rel_date', post).attr('time');
+
+        if (audios.length > 0) {
+          var linkid = 'vkdlzip' + post.id;
+          var link = $('&nbsp;-&nbsp;<a href="#" id="' + linkid + '">Download</a>');
+          $('.wall_text .author,.fw_post_author', post).parent().append(link);
+          link.on('click', function (e) {
+            chrome.runtime.sendMessage({
+              audios: audios,
+              linkid: linkid,
+              tabid: response.tabid,
+              author: author,
+              title: title,
+              time: time
+            });
+            e.preventDefault();
+          });
+        }        
+      });
+      
+	}
+  }, 10);
+});
